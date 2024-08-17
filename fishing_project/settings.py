88a3bf531c74d5a7be7c18c219bd2d9d09e239a4
BeThaYo <delperalxavi@gmail.com>
2024-08-17 20:53:15 +0200

    Preparacion base de datos

--- fishing_project/settings.py
+++ fishing_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'fishing_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '' ,  # o la dirección IP de tu servidor de base de datos
+        'PORT': '3306',  # El puerto por defecto de MySQL
     }
 }
 
