99a62f0ab54c042cc8bda16455159fe95dd08a8d
BeThaYo <delperalxavi@gmail.com>
2024-08-17 19:33:39 +0200

    Clase capturas creada

--- fishing_app/models.py
+++ fishing_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Capture(models.Model):
+    species = models.CharField(max_length=100)
+    size = models.FloatField()
+    weight = models.FloatField()
+    bait = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now_add=True)
