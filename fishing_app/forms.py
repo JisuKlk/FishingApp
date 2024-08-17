a4de683bdb982f54cbbb93c6bbefe829045660a4
JisuKlk <alvarofdez.f10@gmail.com>
2024-08-18 01:24:32 +0200

    Form

--- fishing_app/forms.py
+++ fishing_app/forms.py
@@ -13,8 +13,19 @@ class CustomPasswordResetForm(PasswordResetForm):
 
 # Custom user creation
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+    password2 = forms.CharField(
+        label='Confirm Password',
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'password1', 'password2']
