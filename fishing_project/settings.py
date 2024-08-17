d86668dd8112969f5e14a48ee5e26486278abd13
BeThaYo <delperalxavi@gmail.com>
2024-08-18 00:39:35 +0200

    Base login

--- fishing_project/settings.py
+++ fishing_project/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'fishing_app',
+
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'fishing_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
